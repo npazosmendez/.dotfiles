bdaadfbcae2de25af3ae5fef993e00bfe8622d00
Darío Reyes <eran6reyes@gmail.com>
2017-07-17 22:03:19 -0300

    Firefox setup

--- .init_pack/package_configure.sh
+++ .init_pack/package_configure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Preconditions for succesful configuration
+if [ ! -d "~/apps/firefox" ]; then
+  echo "Error: firefox folder does not exist"
+  exit 1
+fi
+
 # Cd to home
 cd ~/
 
@@ -18,3 +24,17 @@ tmux kill-server   # killing the server is not required, I guess
 # Vim setup
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Firefox setup
+# desktop configuration files involves type recognition, context menu
+#  execution, program classification, etc
+# update-alternatives is used to manage the alternatives' system in
+#  debian based distros, which pretty is a symlink management of
+#  singular links to a group of similar programs (or different
+#  versions of the same program). It's how 'default' applications
+#  work (args: alt_link(generic path), alt_name(sym name in alt dir),
+#  path(specific path), priority(lower=more privilage)
+sudo apt-get remove firefox
+sudo ln -s ~/apps/firefox/firefox /usr/local/bin # firefox visible from PATH
+sudo cp ~/.init_pack/desktop/firefox.desktop /usr/share/applications/ # desktop configuration file
+sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/local/bin/firefox 100
