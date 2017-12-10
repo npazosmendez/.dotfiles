7bc326e755a205424573b503962ea341ae19a553
John Doe <lisa@apartyouretearing.me>
2017-12-10 00:19:14 -0300

    Saca la repo de HOME (gracia' Joaco)$

--- .init/firefox.sh
+++ .init/firefox.sh
@@ -9,22 +9,6 @@ fi
 # Cd to home
 cd ~/
 
-# 'Clone' dotfiles on home directory
-git init .
-git remote add -t \* -f origin https://github.com/eyesmd/dotfiles.git # -f forces a fetch after adding the remote repo, -t idk 
-git checkout master
-
-# Tmux setup
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux start-server   # start a server but don't attach to it
-tmux new-session -d   # create a new session but don't attach to it either
-~/.tmux/plugins/tpm/scripts/install_plugins.sh   # install the plugins
-tmux kill-server   # killing the server is not required, I guess
-
-# Vim setup
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
 # Firefox setup
 # desktop configuration files involves type recognition, context menu
 #  execution, program classification, etc
---
+++ README.md
@@ -0,0 +1,13 @@
+# Dotfiles
+
+To fire up the whole thing run the following:
+
+`git clone git@github.com:eyesmd/dotfiles.git $HOME/.dotfiles && $HOME/.dotfiles/.init/install`
+
+## Todo
+ * Modularize 'install' script
+ * Source bash_user somewhere within 'install'
+ * Add to .bin to PATH
+ * Handle more gracefully the case where tmux and vim folders already exist
+ * Firefox script
+ * Colors, bitch
